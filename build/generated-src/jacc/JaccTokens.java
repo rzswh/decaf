94afcd1ecda889519cafdbbfd0a1e55e2e07f237
dflasher <rzswh@163.com>
2019-10-08 08:58:33 +0800

    initial commit

---
+++ build/generated-src/jacc/JaccTokens.java
@@ -0,0 +1,57 @@
+// Output created by jacc 2.1.0
+package decaf.frontend.parsing;
+
+interface JaccTokens {
+    int ENDINPUT = 0;
+    int AND = 1;
+    int BOOL = 2;
+    int BOOL_LIT = 3;
+    int BREAK = 4;
+    int CLASS = 5;
+    int ELSE = 6;
+    int EMPTY = 7;
+    int EQUAL = 8;
+    int EXTENDS = 9;
+    int FOR = 10;
+    int GREATER_EQUAL = 11;
+    int IDENTIFIER = 12;
+    int IF = 13;
+    int INSTANCE_OF = 14;
+    int INT = 15;
+    int INT_LIT = 16;
+    int LESS_EQUAL = 17;
+    int NEW = 18;
+    int NOT_EQUAL = 19;
+    int NULL = 20;
+    int OR = 21;
+    int PRINT = 22;
+    int READ_INTEGER = 23;
+    int READ_LINE = 24;
+    int RETURN = 25;
+    int STATIC = 26;
+    int STRING = 27;
+    int STRING_LIT = 28;
+    int THIS = 29;
+    int UMINUS = 30;
+    int VOID = 31;
+    int WHILE = 32;
+    int error = 34;
+    // '!' (code=33)
+    // '%' (code=37)
+    // '(' (code=40)
+    // ')' (code=41)
+    // '*' (code=42)
+    // '+' (code=43)
+    // ',' (code=44)
+    // '-' (code=45)
+    // '.' (code=46)
+    // '/' (code=47)
+    // ';' (code=59)
+    // '<' (code=60)
+    // '=' (code=61)
+    // '>' (code=62)
+    // '[' (code=91)
+    // ']' (code=93)
+    // '{' (code=123)
+    // '}' (code=125)
+}
